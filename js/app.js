0024f284574e179430b5422ba89144a0d1ca23a0
AyaaBe95 <75928500+AyaaBe95@users.noreply.github.com>
2020-12-29 09:47:34 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 var userInput = prompt("Enter your name")
 alert("Welcome "+ userInput+ " to my gusseing game")
-console.log(userInput)
+//console.log(userInput)
 
 var userInput1 = prompt("How are you feeling today? "+userInput); 
  
@@ -26,7 +26,7 @@ alert("Great! you are right")
 else{
     alert("Wrong answer,I love reading")
 }
-console.log(userInput2)
+//console.log(userInput2)
 
 var userInput3=prompt("Do I love programming ? answer (yes/no or y/n) ")
 if (userInput3.toLocaleLowerCase == "yes" || "y" ){
@@ -43,7 +43,7 @@ alert("Exactly,I`m a girl")
 else{
     alert("Oops!, Wrong answer")
 }
-console.log(userInput4)
+//console.log(userInput4)
 
 var userInput5=prompt("Did I graduated in 2017 ? answer (yes/no or y/n) ")
 if (userInput5.toLocaleLowerCase == "yes" || "y" ){
@@ -52,7 +52,7 @@ alert("Wrong answer,I graduated in 2018")
 else{
     alert("You're right,I didnt graduated in 2017")
 }
-console.log(userInput5)
+//console.log(userInput5)
 
 var userInput6=prompt("Do I want to take the master degree ? answer (yes/no or y/n) ")
 if (userInput6.toLocaleLowerCase == "yes" || "y" ){
@@ -61,4 +61,4 @@ alert("Great! you are right")
 else{
     alert("Wrong answer,I want to complete postgraduate studies")
 }
-console.log(userInput6)
+//console.log(userInput6)
